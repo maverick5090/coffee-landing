1a7c06886781d75f752ed4fe4400f5a41c1f2521
maverick5090 <deveshagre50@gmail.com>
2026-03-16 22:39:53 +0530

    Updates

---
+++ _next/static/KqKMCKYicW9cP0sVV8-iN/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/coffee-landing//_next/:path+",
+        "destination": "/coffee-landing/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
